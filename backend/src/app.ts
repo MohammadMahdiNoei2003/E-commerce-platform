a2d9828c991a19f4a918668f3c54c106d7c3ddc5
Mohammad Mahdi Noei <noeimohammadmahdi@gamil.com>
2025-01-06 19:41:34 +0330

    add cart, product, category and price entities to database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 import userRouter from './ui/routes/User.Routes';
 import authRouter from './ui/routes/Auth.Routes';
+import productRouter from './ui/routes/Product.Routes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
+app.use('/product', productRouter);
 
 app.use(errorHandler);
 
