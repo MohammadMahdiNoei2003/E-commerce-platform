291a228552d527473045c74e100dfe3b1af58373
Mohammad Mahdi Noei <noeimohammadmahdi@gamil.com>
2025-02-12 21:38:22 +0330

    add address, cart, order, payment and shipping routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,12 @@ import userRouter from './presentation/routes/User.Routes';
 import authRouter from './presentation/routes/Auth.Routes';
 import productRouter from './presentation/routes/Product.Routes';
 import categoryRouter from './presentation/routes/Category.Routes';
+import addressRouter from './presentation/routes/Address.Routes';
+import cartRouter from './presentation/routes/Cart.Routes';
+import orderRouter from "./presentation/routes/Order.Routes";
+import paymentRouter from "./presentation/routes/Payment.Routes";
+import shippingRouter from "./presentation/routes/Shipping.Routes";
+
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
@@ -14,6 +20,11 @@ app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/product', productRouter);
 app.use('/category', categoryRouter);
+app.use('/address', addressRouter);
+app.use('/cart', cartRouter);
+app.use('/order', orderRouter);
+app.use('/payment', paymentRouter);
+app.use('/shipping', shippingRouter);
 
 app.use(errorHandler);
 
